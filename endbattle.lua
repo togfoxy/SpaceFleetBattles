2609a71e2a3a6487a259741e821004c76865fd81
Fox <togfoxy@users.noreply.github.com>
2023-05-05 15:15:49 +1000

    Office
    Office

---
+++ endbattle.lua
@@ -0,0 +1,86 @@
+endbattle = {}
+
+--! be sure to clear the squad_list and OBJECTS at the end of eacy battle
+--! be sure to take pilots out of fighters
+--!fightsceneHasLoaded = false
+
+function endbattle.draw()
+    love.graphics.setColor(1,1,1,1)
+    local txt = "Friendly ships lost: " .. SCORE.friendsdead
+    love.graphics.print(txt, 100, 100)
+    local txt = "Enemy ships destroyed: " .. SCORE.enemiesdead
+    love.graphics.print(txt, 100, 150)
+
+    if fun.isPlayerAlive() then
+        txt = "Player survived"
+    else
+        txt = "Player was lost in battle"
+    end
+    love.graphics.print(txt, 100, 200)
+end
+
+function endbattle.loadButtons()
+    -- call this from love.load()
+    -- ensure buttons.drawButtons() is added to the scene.draw() function
+    -- ensure scene.mousereleased() function is added
+
+    -- local numofbuttons = 2      -- how many buttons on this form, assuming a single column
+    -- local numofsectors = numofbuttons + 1
+    -- local buttonsequence = 1            -- sequence on the screen
+
+    -- button for new game
+    local mybutton = {}
+    mybutton.x = (SCREEN_WIDTH / 2) - 75
+    mybutton.y = SCREEN_HEIGHT / 2 - 100
+    mybutton.width = 175
+    mybutton.height = 25
+    mybutton.bgcolour = {169/255,169/255,169/255,1}
+    mybutton.drawOutline = false
+    mybutton.outlineColour = {1,1,1,1}
+    mybutton.label = "New game"
+    mybutton.image = nil
+    mybutton.imageoffsetx = 20
+    mybutton.imageoffsety = 0
+    mybutton.imagescalex = 0.9
+    mybutton.imagescaley = 0.3
+    mybutton.labelcolour = {1,1,1,1}
+    mybutton.labeloffcolour = {1,1,1,1}
+    mybutton.labeloncolour = {1,1,1,1}
+    mybutton.labelcolour = {0,0,0,1}
+    mybutton.labelxoffset = 40
+
+    mybutton.state = "on"
+    mybutton.visible = true
+    mybutton.scene = enum.sceneEndBattle             -- change and add to enum
+    mybutton.identifier = enum.buttonEndBattleNewGame     -- change and add to enum
+    table.insert(GUI_BUTTONS, mybutton) -- this adds the button to the global table
+
+    -- button for exit game
+    local mybutton = {}
+    mybutton.x = (SCREEN_WIDTH / 2) - 75
+    mybutton.y = SCREEN_HEIGHT / 2 + 150
+    mybutton.width = 175
+    mybutton.height = 25
+    mybutton.bgcolour = {169/255,169/255,169/255,1}
+    mybutton.drawOutline = false
+    mybutton.outlineColour = {1,1,1,1}
+    mybutton.label = "Exit game"
+    mybutton.image = nil
+    mybutton.imageoffsetx = 20
+    mybutton.imageoffsety = 0
+    mybutton.imagescalex = 0.9
+    mybutton.imagescaley = 0.3
+    mybutton.labelcolour = {1,1,1,1}
+    mybutton.labeloffcolour = {1,1,1,1}
+    mybutton.labeloncolour = {1,1,1,1}
+    mybutton.labelcolour = {0,0,0,1}
+    mybutton.labelxoffset = 40
+
+    mybutton.state = "on"
+    mybutton.visible = true
+    mybutton.scene = enum.sceneEndBattle             -- change and add to enum
+    mybutton.identifier = enum.buttonEndBattleExitGame     -- change and add to enum
+    table.insert(GUI_BUTTONS, mybutton) -- this adds the button to the global table
+end
+
+return endbattle
---
+++ main.lua
@@ -0,0 +1,199 @@
+GAME_VERSION = "0.01"
+
+inspect = require 'lib.inspect'
+-- https://github.com/kikito/inspect.lua
+
+res = require 'lib.resolution_solution'
+-- https://github.com/Vovkiv/resolution_solution
+
+Camera = require 'lib.cam11.cam11'
+-- https://notabug.org/pgimeno/cam11
+
+bitser = require 'lib.bitser'
+-- https://github.com/gvx/bitser
+
+nativefs = require 'lib.nativefs'
+-- https://github.com/EngineerSmith/nativefs
+
+lovelyToasts = require 'lib.lovelyToasts'
+-- https://github.com/Loucee/Lovely-Toasts
+
+anim8 = require 'lib.anim8'
+-- https://github.com/kikito/anim8
+
+-- these are core modules
+require 'lib.buttons'
+require 'enums'
+require 'constants'
+fun = require 'functions'
+cf = require 'lib.commonfunctions'
+
+require 'mainmenu'
+require 'fight'
+require 'endbattle'
+require 'commanderai'
+require 'squadai'
+require 'unitai'
+require 'fighter'
+
+function love.resize(w, h)
+	res.resize(w, h)
+end
+
+function beginContact(a, b, coll)
+	-- a and be are fixtures
+	-- get the body that owns the fixture
+	local object1index, object2index
+
+	for i = 1, #OBJECTS do
+		if OBJECTS[i].fixture == a then
+			object1index = i
+		end
+		if OBJECTS[i].fixture == b then
+			object2index = i
+		end
+	end
+
+	local victim = {}			-- this will contain the fighter object that was hit
+	local bullet = {}
+	if OBJECTS[object1index].body:isBullet() then
+		-- destroy Obj2
+		victim = OBJECTS[object2index]
+		bullet = OBJECTS[object1index]
+		OBJECTS[object1index].lifetime = 0
+	end
+	if OBJECTS[object2index].body:isBullet() then
+		-- destroy Obj1
+		victim = OBJECTS[object1index]
+		bullet = OBJECTS[object2index]
+		OBJECTS[object2index].lifetime = 0
+	end
+
+	fun.applyDamage(victim, bullet)		-- assumes bullet hit fighter
+
+	-- play sounds if player is hit  		--! what about explosion if dead?
+	if victim.guid == PLAYER_GUID then
+		cf.playAudio(enum.audioBulletPing, false, true)
+	end
+end
+
+function endContact(a, b, coll)
+end
+
+function love.keyreleased( key, scancode )
+	if key == "escape" then
+		cf.removeScreen(SCREEN_STACK)
+	end
+
+	local currentscene = cf.currentScreenName(SCREEN_STACK)
+	if currentscene == enum.sceneFight then
+		fight.keyreleased(key, scancode)
+	end
+end
+
+function love.wheelmoved(x, y)
+	local currentscene = cf.currentScreenName(SCREEN_STACK)
+	if currentscene == enum.sceneFight then
+		fight.wheelmoved(x, y)
+	end
+end
+
+function love.mousemoved(x, y, dx, dy, istouch )
+	local currentscene = cf.currentScreenName(SCREEN_STACK)
+	if currentscene == enum.sceneFight then
+		fight.mousemoved(x, y, dx, dy)
+	end
+end
+
+function love.mousereleased(x, y, button, isTouch)
+	local rx, ry = res.toGame(x,y)
+	local currentscene = cf.currentScreenName(SCREEN_STACK)
+
+	if currentscene == enum.sceneFight then
+		fight.mousereleased(rx, ry, x, y, button)		-- need to send through the res adjusted x/y and the 'real' x/y
+	elseif currentscene == enum.scenePodium then
+	elseif currentscene == enum.sceneMainMenu then
+		mainmenu.mousereleased(rx, ry, x, y, button)
+	end
+end
+
+function love.load()
+
+	res.init({width = 1920, height = 1080, mode = 2})
+
+	local _, _, flags = love.window.getMode()
+	local width, height = love.window.getDesktopDimensions(flags.display)
+	-- local width, height = love.window.getDesktopDimensions(1)
+	res.setMode(width, height, {resizable = true})
+
+	constants.load()		-- also loads enums
+	fun.loadFonts()
+    fun.loadAudio()
+	fun.loadImages()
+
+	mainmenu.loadButtons()
+	battleroster.loadButtons()
+	endbattle.loadButtons()
+
+	cam = Camera.new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 1)
+	cam:setZoom(ZOOMFACTOR)
+	cam:setPos(TRANSLATEX,	TRANSLATEY)
+
+	love.window.setTitle("Dogfight 2 " .. GAME_VERSION)
+
+	love.keyboard.setKeyRepeat(true)
+
+	cf.addScreen(enum.sceneMainMenu, SCREEN_STACK)
+	-- cf.addScreen(enum.sceneFight, SCREEN_STACK)
+
+	lovelyToasts.canvasSize = {SCREEN_WIDTH, SCREEN_HEIGHT}
+	lovelyToasts.options.tapToDismiss = true
+	lovelyToasts.options.queueEnabled = true
+
+	---------------
+	love.physics.setMeter(30)
+	PHYSICSWORLD = love.physics.newWorld(0,0,false)
+	PHYSICSWORLD:setCallbacks( beginContact, endContact, preSolve, postSolve )
+
+	GRIDS[enum.gridExplosion] = anim8.newGrid(16, 16, IMAGE[enum.imageExplosion]:getWidth(), IMAGE[enum.imageExplosion]:getHeight())
+end
+
+function love.draw()
+    res.start()
+	local currentscene = cf.currentScreenName(SCREEN_STACK)
+	if currentscene == enum.sceneFight then
+		fight.draw()
+	elseif currentscene == enum.sceneMainMenu then
+		mainmenu.draw()
+	elseif currentscene == enum.sceneEndBattle then
+		endbattle.draw()
+	else
+		error()
+	end
+
+	-- draw animations
+	love.graphics.setColor(1,1,1,1)
+	local scale = love.physics.getMeter( )
+	for _, animation in pairs(ANIMATIONS) do
+		local drawx, drawy = cam:toScreen(animation.drawx, animation.drawy)
+		if animation.type == enum.animExplosion then
+			animation:draw(IMAGE[enum.imageExplosion], drawx, drawy, animation.angle, 1, 1, 0, 0)
+		elseif animation.type == enum.animSmoke then
+			-- different offset
+			animation:draw(IMAGE[enum.imageExplosion], drawx, drawy, animation.angle, 1, 1, 10, 0)
+		end
+	end
+
+    res.stop()
+end
+
+
+function love.update(dt)
+	local currentscene = cf.currentScreenName(SCREEN_STACK)
+	if currentscene == enum.sceneFight then
+        fight.update(dt)
+	end
+
+	fun.updateAnimations(dt)
+
+end
